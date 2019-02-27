70aed45dc3c22c0004011a21edaac6d36ca76fb8
visualstudio <visualstudio@122-PC23>
2019-02-27 10:07:06 -0500

    final commit for today

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,8 +11,38 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             var orderProcessor = new OrderProcessor();
-            var order = new Order { DatePlaced = DateTime.Now, TotalPrice = 100f};
+            var order = new Order { DatePlaced = DateTime.Now, TotalPrice = 100f };
             orderProcessor.Process(order);
         }
     }
+    public class OrderProcessor
+    {
+        private readonly ShippingCalculator _shippingCalculator;
+        public OrderProcessor()
+        {
+            _shippingCalculator;ator = new ShippingCalculator();
+        }
+        public void Process(Order order)
+        {
+            if (order.Isshipped)
+                throw new InvalidOperationException("This order is already processed.");
+            order.Shipment=new shipment
+            {
+                Cost=_shippingCalculator.CalculateShipping(order),
+                ShippingDate=DateTime.Today.AddDays(1)
+            }
+        }
+    }
+    public class ShippingCalculator
+    {
+        public float CalculateShipping(Order order)
+        {
+            if (order.TotalPrice < 30f)
+                return order.TotalPrice * 0.1f;
+            return 0;
+        }
+    }
+
+
 }
+
---
+++ c0732442.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("c0732442.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("c0732442.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("41c5f0d1-fb54-4d9a-ae57-af34abe2227c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
